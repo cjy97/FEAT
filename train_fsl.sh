a336ca7decb9a7c207d21d10a76f486e073985d8
cjy97 <857369174@qq.com>
2022-01-10 11:17:18 +0800

    1

--- train_fsl.sh
+++ train_fsl.sh
@@ -2,14 +2,14 @@ python train_fsl.py  \
 --max_epoch 200 \
 --model_class FEAT \
 --backbone_class Res12 \
---dataset MiniImageNet \
+--dataset TieredImageNet \
 --way 5 \
 --eval_way 5 \
 --shot 1 \
 --eval_shot 1 \
 --query 15 \
 --eval_query 15 \
---balance 0.01 \
+--balance 0.1 \
 --temperature 64 \
 --temperature2 64 \
 --lr 0.0002 \
@@ -17,6 +17,6 @@ python train_fsl.py  \
 --lr_scheduler step \
 --step_size 40 --gamma 0.5 \
 --gpu 1 \
---init_weights ./saves/initialization/miniimagenet/Res12-pre.pth \
+--init_weights ./saves/initialization/tieredimagenet/Res12-pre.pth \
 --eval_interval 1 \
 --use_euclidean
